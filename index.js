ce851a4d2f39e085c7b38b87765cb74a897745e0
Erick Ochieng <erickochieng766@gmail.com>
2022-11-23 13:34:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    let temp = [...cats]
+    temp.push(name)
+    return temp
+}
+function prependCat(name){
+    let temp = [...cats]
+    temp.unshift(name)
+    return temp
+}
+function removeLastCat(){
+    let temp = [...cats]
+    temp.pop()
+    return temp
+}
+function removeFirstCat(){
+    let temp = [...cats]
+    temp.shift()
+    return temp
+}
